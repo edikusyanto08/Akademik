a3e65258ec45f440907027b0e438b5e3c0b24cbc
= <=>
2015-08-08 01:22:28 +0800

    ini yang baru.. ada data diklat dan pekerjaan dll

--- resources/views/stafftu/pengaturan/hobi/form.blade.php
+++ resources/views/stafftu/pengaturan/hobi/form.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
 	<div class="row">
 	<div class="col-md-12">
-	<div class="col-md-6">
+	<div class="col-md-12">
 		<div class="form-group floating-label">
 			{!!Form::text('label',null,['class'=>'form-control', 'id'=>'label'])!!}
 			{!!Form::label('label','Nama Hobi')!!}
